7329e5f923d7ae364584de28984d06e7e228f229
aayush jain <aayushjain1290@gmail.com>
2024-09-28 16:03:50 +0530

    jai bolo ambe ma

--- src/contollers/VideoController.js
+++ src/contollers/VideoController.js
@@ -2,7 +2,14 @@ import { uploadOnCloudinary } from '../utils/cloudnary.js';
 import { Video } from '../models/Video.model.js';
 import mongoose from 'mongoose';
 import { User } from '../models/user.model.js';
-
+import { v2 as cloudinary } from "cloudinary";
+import streamifier from 'streamifier';
+// Cloudinary configuration
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const uploadVideo = async (req, res, next) => {
     try {
