05731cc7a462ebbc59e7377c4cc4ac50d5f8016c
aayush jain <aayushjain1290@gmail.com>
2024-10-26 22:00:46 +0530

    lk

--- src/contollers/registeruser.js
+++ src/contollers/registeruser.js
@@ -55,7 +55,7 @@ const uploadToCloudinary = (fileBuffer, resourceType) => {
     try {
         // Check if the user already exists
         const existedUser = await User.findOne({
-            $or: [{email}]
+            $or: [{username},{email}]
         });
 
         if (existedUser) {
