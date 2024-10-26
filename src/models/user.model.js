9581cb753365e81a906130641e9c5db2baee2f88
aayush jain <aayushjain1290@gmail.com>
2024-10-26 18:50:32 +0530

    mou

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,15 +6,11 @@ const userSchema = new Schema(
         username: {
             type: String,
             required: true,
-            unique: true,
-           
             index: true
         },
         email: {
             type: String,
             required: true,
-            unique: true,
-           
         },
         fullName: {
             type: String,
